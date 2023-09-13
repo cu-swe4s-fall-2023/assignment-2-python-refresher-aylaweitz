a5819e1245d5d323032b8f310ff3477c976c933f
Ayla Weitz <aylaweitz@engr2-2-83-218-dhcp.int.colorado.edu>
2023-09-13 12:02:20 -0600

    updated print_fires to access correct columns

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,9 @@
+from my_utils import get_column
+
+# print the number of fires in the United States of America
 country='United States of America'
-county_column = 1
-fires_column = 4
+country_column = 0 # first column corresponds to the 0th index
+fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
+fires = get_column(file_name, country_column, country, fires_column)
 print(fires)
