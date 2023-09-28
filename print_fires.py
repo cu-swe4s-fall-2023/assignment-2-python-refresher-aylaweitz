37cad2e11ef23761264ce2e2298d49ef26015463
Ayla Weitz <aylaweitz@Aylas-MacBook-Pro.local>
2023-09-28 16:15:19 +0900

    argparse so print_fires takes command line args

--- print_fires.py
+++ print_fires.py
@@ -1,9 +1,32 @@
 from my_utils import get_column
+import argparse
 
-# print the number of fires in the United States of America
-country='United States of America'
-country_column = 0 # first column corresponds to the 0th index
-fires_column = 3
-file_name = 'Agrofood_co2_emission.csv'
-fires = get_column(file_name, country_column, country, fires_column)
+parser = argparse.ArgumentParser(description='Pass in parameters for fire search.')
+
+parser.add_argument('--country', 
+                    type=str, 
+                    default='United States of America', # default to searching for fires in US
+                    help='Name of the country of interest.', 
+                    required=False)
+
+parser.add_argument('--country_column', 
+                    type=int, 
+                    default=0, 
+                    help='Index of country column in file.', 
+                    required=False)
+
+parser.add_argument('--fires_column', 
+                    type=int, 
+                    default=3, 
+                    help='Index of fires column in file.', 
+                    required=False)
+
+parser.add_argument('--file_name', # 'Agrofood_co2_emission.csv'
+                    type=str, 
+                    help='Name of the file to analyze.', 
+                    required=True)
+
+args = parser.parse_args()
+
+fires = get_column(args.file_name, args.country_column, args.country, args.fires_column)
 print(fires)
