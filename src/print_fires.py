573514278826d5343d1de922f8a7b12933ba2736
Ayla Weitz <aylaweitz@cu-biot-13-10.203.176.151.int.colorado.edu>
2023-10-10 16:36:36 -0600

    implemented pycodestyle

--- src/print_fires.py
+++ src/print_fires.py
@@ -2,43 +2,49 @@ import my_utils
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='Pass in parameters for fire search.')
+parser = argparse.ArgumentParser(
+    description='Pass in parameters for fire search.'
+    )
 
-parser.add_argument('--country', 
-                    type=str, 
-                    default='United States of America', # default to searching for fires in US
-                    help='Name of the country of interest.', 
+parser.add_argument('--country',
+                    type=str,
+                    default='United States of America',  # US default
+                    help='Name of the country of interest.',
                     required=False)
 
-parser.add_argument('--country_column', 
-                    type=int, 
-                    default=0, 
-                    help='Index of country column in file.', 
+parser.add_argument('--country_column',
+                    type=int,
+                    default=0,
+                    help='Index of country column in file.',
                     required=False)
 
-parser.add_argument('--fires_column', 
-                    type=int, 
-                    default=3, 
-                    help='Index of fires column in file.', 
+parser.add_argument('--fires_column',
+                    type=int,
+                    default=3,
+                    help='Index of fires column in file.',
                     required=False)
 
-parser.add_argument('--file_name', # 'Agrofood_co2_emission.csv'
-                    type=str, 
-                    help='Name of the file to analyze.', 
+parser.add_argument('--file_name',  # 'Agrofood_co2_emission.csv'
+                    type=str,
+                    help='Name of the file to analyze.',
                     required=True)
 
 parser.add_argument('--operation',
-                    type=str, 
+                    type=str,
                     default=None,
-                    help='Operation to implement (mean/median/standard deviation)', 
+                    help='Operation to implement',
                     required=False)
 
 args = parser.parse_args()
 
-def main():
-    fires = my_utils.get_column(args.file_name, args.country_column, args.country, args.fires_column)
 
-    if args.operation: # if an operation is given
+def main():
+    fires = my_utils.get_column(args.file_name,
+                                args.country_column,
+                                args.country,
+                                args.fires_column)
+
+    if args.operation:  # if an operation is given
         if args.operation == 'mean':
             print(f'Mean: {my_utils.calc_mean(fires)}')
         elif args.operation == 'median':
@@ -47,11 +53,12 @@ def main():
             print(f'Std: {my_utils.calc_std(fires)}')
 
         else:
-            print(f'"{args.operation}" is an unrecognized operation name. Try something else.')
+            print(f'"{args.operation}" is an unrecognized operation name.')
             sys.exit(5)
 
     else:
         print(fires)
+
 
 if __name__ == '__main__':
     main()
